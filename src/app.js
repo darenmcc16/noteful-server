e7163be47330c97e07e653dd590f424e79bfcce2
Daren McCarrellgit config --global user.email darenmcc16@gmail.com <darenmcc16@gmail.com>
2021-02-02 21:21:34 -0600

    complete

--- src/app.js
+++ src/app.js
@@ -7,8 +7,6 @@ const {NODE_ENV} = require('./config')
 
 const app = express()
 
-
-
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
@@ -18,18 +16,18 @@ app.use(helmet())
 app.use(cors())
 
 app.get('/', (req, res) =>{
-  res.send('hello, world')
+  res.send('Hello, world!')
 })
 
 app.use(function errorHandler(error, req, res, next){
   let response
-  if (NODE_ENV === 'production'){
-    response = { error: { message: 'server error' } }
+  if(NODE_ENV === 'production'){
+    response = {error: {message: 'server error'}}
   }else{
-      console.error(error)
-      response = { message: error.message, error }
-    }
-    res.status(500).json(response)
+    console.error(error)
+    response = {message: error.message, error}
+  }
+  res.status(500).json(response)
 })
 
 module.exports = app
