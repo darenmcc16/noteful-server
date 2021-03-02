5712ddc44dc9d80e5b470f159c55acc6f43ac149
Daren McCarrellgit config --global user.email darenmcc16@gmail.com <darenmcc16@gmail.com>
2021-03-01 19:02:16 -0600

    test

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-    DB_URL: 'postgres://tklqjdmfiziqdi:464df57cba46861c2a42fd5a59222df8f298fbe3ab4cbe432170e04b8c87226d@ec2-52-70-67-123.compute-1.amazonaws.com:5432/de9lrfo3hndgtj',
+    DB_URL: process.env.DB_URL || 'postgres://tklqjdmfiziqdi:464df57cba46861c2a42fd5a59222df8f298fbe3ab4cbe432170e04b8c87226d@ec2-52-70-67-123.compute-1.amazonaws.com:5432/de9lrfo3hndgtj',
   }
