0a5817007bd293e3127ccb36e637ab7e6e1409ba
Daren McCarrellgit config --global user.email darenmcc16@gmail.com <darenmcc16@gmail.com>
2021-03-04 18:19:02 -0600

    added url to config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgres://tklqjdmfiziqdi:464df57cba46861c2a42fd5a59222df8f298fbe3ab4cbe432170e04b8c87226d@ec2-52-70-67-123.compute-1.amazonaws.com:5432/de9lrfo3hndgtj',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://tklqjdmfiziqdi:464df57cba46861c2a42fd5a59222df8f298fbe3ab4cbe432170e04b8c87226d@ec2-52-70-67-123.compute-1.amazonaws.com:5432/de9lrfo3hndgtj',
   }
