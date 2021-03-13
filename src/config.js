30d8b01155b67c0c8b0fd82a1343ddb9d379c7b7
Daren McCarrellgit config --global user.email darenmcc16@gmail.com <darenmcc16@gmail.com>
2021-03-12 20:18:09 -0600

    updated database

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     SSL:true,
-    //DATABASE_URL: process.env.DATABASE_URL || 'postgres://owuutryuiyqohe:a337065a1c8feb655303559be30e1d700591df2196d13612cec8d7b1fbd2f69e@ec2-18-204-74-74.compute-1.amazonaws.com:5432/djudlcjsp0ede',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgres://dunder_mifflin:Whitesox1$@localhost:5432/noteful',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://joaenexzpevool:9d1ef4c83cd8aa788c8329cb14b6de8df5b690a61b965031172a7995b7fc144e@ec2-54-159-175-113.compute-1.amazonaws.com:5432/d9g4nikl2rm9fs',
+    //DATABASE_URL: process.env.DATABASE_URL || 'postgres://dunder_mifflin:Whitesox1$@localhost:5432/noteful',
   }
